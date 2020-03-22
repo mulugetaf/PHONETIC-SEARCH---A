356959f1e7a507d7174c54a4caceb15197dda312
Mulugeta <muluks@gmail.com>
2020-03-22 17:24:57 +0200

    first commit

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,5 @@
+#include "PhoneticFinder.hpp"
+
+namespace phonetic{
+string find(string s1,string s2){ return "";};
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+
+namespace phonetic{
+string find(string s1,string s2);
+}
